5a9fffa815b8de400889206cff9ffb6ddb967c30
NobodyKnowNothing <144275664+NobodyKnowNothing@users.noreply.github.com>
2026-04-04 20:48:37 -0400

    Newtonian dynamics completed and barnes-hutt started

---
+++ cpp/barnes-hut.cpp
@@ -0,0 +1,13 @@
+
+typedef struct {
+    double CoMx = 0
+    double CoMy = 0
+    double totalMass = 0;
+    struct qtnode NW = NULL;
+    struct qtnode NE = NULL;
+    struct qtnode SW = NULL;
+    struct qtnode SE = NULL;
+
+} qtnode
+
+inline void 
